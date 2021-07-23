937c0e81137515eb00a286a70b43019fc069a581
Sergey <serridd@gmail.com>
2021-07-23 21:19:57 +0300

    Migrate code to use State monad and MaybeT transformer.

--- src/App.hs
+++ src/App.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module App where
@@ -7,6 +8,10 @@ import Data.Function
 import Data.Maybe
 import Data.IORef
 import Control.Monad
+--import Control.Monad.State.Lazy
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class (lift)
 
 import GHC.OldList (find)
 
@@ -33,15 +38,20 @@ data AppState = AppState
     , as'continueEh :: Bool
     }
 
+type Handler = App.UpdateHandler AppState
+
 mapMode :: (Mode -> Mode) -> AppState -> AppState
 mapMode f state = state { as'mode = (f $ as'mode state) }
 
 voidIO :: (b -> IO a) -> (b -> IO ())
 voidIO k = void . k
 
-handleRust :: App.UpdateHandler AppState
-handleRust = App.UpdateHandler $ App.makeStateful $ \update ->
-    fromMaybe [] (actions update)
+hoistMaybe :: Monad m => Maybe a -> MaybeT m a
+hoistMaybe = MaybeT . pure
+
+handleRust :: Handler
+handleRust = App.UpdateHandler $ \update -> do
+    pure $ fromMaybe [] (actions update)
     where
     actions :: Model.Update -> Maybe [Api.ApiAction]
     actions update = do
@@ -59,9 +69,10 @@ handleRust = App.UpdateHandler $ App.makeStateful $ \update ->
  
     forbiddenWords = ["раст", "Rust", "rust"]
 
-handleStickerInfo :: App.UpdateHandler AppState
-handleStickerInfo = App.UpdateHandler $
-        \(state, upd) -> (mapMode (const Start) state, fromMaybe [] $ actions upd)
+handleStickerInfo :: Handler
+handleStickerInfo = App.UpdateHandler $ \update -> do
+    modify $ mapMode (const Start)
+    pure (fromMaybe [] $ actions update)
     where
     actions :: Model.Update -> Maybe [Api.ApiAction]
     actions update = do
@@ -74,63 +85,69 @@ handleStickerInfo = App.UpdateHandler $
         let str = "file_id: " `T.append` file
         pure [voidIO $ Api.requestSendMessage $ Argument.sendMessageWithReply chatid str messageid]
 
-handleAddEmoji :: App.UpdateHandler AppState
-handleAddEmoji = App.UpdateHandler $
-        \(state, upd) -> fromMaybe (state, []) $ actions state upd
+handleAddEmoji :: Handler
+handleAddEmoji = App.UpdateHandler $ \update ->
+    fmap (fromMaybe []) $ runMaybeT (act update)
     where
-    actions :: AppState -> Model.Update -> Maybe (AppState, [Api.ApiAction])
-    actions state update = do
-        message <- Model.message update
-        sticker <- Model.sticker message
+    act :: Model.Update -> MaybeT (State AppState) [Api.ApiAction]
+    act update = do
+        message <- hoistMaybe $ Model.message update
+        sticker <- hoistMaybe $ Model.sticker message
         let file = Model.file_id sticker
         let chatid = Model.chat'id $ Model.chat $ message
         let messageid = Model.message_id message
 
+        state <- lift $ get
         let (WaitingAddEmoji emojitag) = as'mode state
 
-        let persistence = as'persistence state
-        let emojiBindings = Persistence.p'emojiBindings persistence
-
-        let newState = state {
+        -- modify state
+        lift $ modify (\state ->
+            let persistence = as'persistence state in
+            let emojiBindings = Persistence.p'emojiBindings persistence in
+            state {
             as'mode = Start,
             as'persistence = persistence
                 { Persistence.p'emojiBindings = Map.insert (T.pack emojitag) file emojiBindings
                 }
             }
+            )
 
         let str = "Emoji " `T.append` (T.pack emojitag) `T.append` " added!"
-        pure (newState, [voidIO $ Api.requestSendMessage $ Argument.sendMessageWithReply chatid str messageid])
+        pure [voidIO $ Api.requestSendMessage $ Argument.sendMessageWithReply chatid str messageid]
+        
 
-handleEmoji :: App.UpdateHandler AppState
-handleEmoji = App.UpdateHandler $
-        \(state, upd) -> fromMaybe (state, []) $ actions state upd
+handleEmoji :: Handler
+handleEmoji = App.UpdateHandler $ \update ->
+    fmap (fromMaybe []) $ runMaybeT (act update)
     where
-    actions :: AppState -> Model.Update -> Maybe (AppState, [Api.ApiAction])
-    actions state update = do
-        message <- Model.message update
+    act :: Model.Update -> MaybeT (State AppState) [Api.ApiAction]
+    act update = do
+        message <- hoistMaybe $ Model.message update
         let chatid = Model.chat'id $ Model.chat $ message
         let messageid = Model.message_id message
-        text <- Model.text message
+        text <- hoistMaybe $ Model.text message
 
-        let persistence = as'persistence state
+        persistence <- lift $ gets as'persistence
         let emojiBindings = Persistence.p'emojiBindings persistence
 
-        file <- Map.lookup text emojiBindings
+        file <- hoistMaybe $ Map.lookup text emojiBindings
 
-        pure (state, [voidIO $ Api.requestSendSticker $ Argument.SendSticker chatid file])
+        pure [voidIO $ Api.requestSendSticker $ Argument.SendSticker chatid file]
 
-handleCommand :: App.UpdateHandler AppState
-handleCommand = App.UpdateHandler $ \(state, upd) -> fromMaybe (state, []) (go state upd)
+handleCommand :: Handler
+handleCommand  = App.UpdateHandler $ \update ->
+    fmap (fromMaybe []) $ runMaybeT (act update)
     where
-    go :: AppState -> Model.Update -> Maybe (AppState, [Api.ApiAction])
-    go state update = do
-        message <- Model.message update
-        text <- Model.text message
+    act :: Model.Update -> MaybeT (State AppState) [Api.ApiAction]
+    act update = do
+        message <- hoistMaybe $ Model.message update
+        text <- hoistMaybe $ Model.text message
         let chatid = Model.chat'id $ Model.chat $ message
         let messageid = Model.message_id message
 
-        (newMode, str) <- commandMapping text
-        pure (mapMode (const newMode) state, [voidIO $ Api.requestSendMessage $ Argument.sendMessageWithReply chatid str messageid])
+        (newMode, str) <- hoistMaybe $ commandMapping text
+        lift $ modify (mapMode $ const newMode)
+        pure [voidIO $ Api.requestSendMessage $ Argument.sendMessageWithReply chatid str messageid]
 
 commandMap :: Map.Map String (String -> (Mode, Text))
 commandMap = Map.fromList
@@ -149,15 +166,17 @@ commandMapping text =
 mapText :: (String -> String) -> Text -> Text
 mapText f = T.pack . f . T.unpack
 
-handleText :: App.UpdateHandler AppState
+handleText :: Handler
 handleText = handleRust <> handleEmoji  <> handleCommand
 
 -- Choose handler based on AppState
-handler :: App.UpdateHandler AppState
-handler = App.UpdateHandler $ \o@(state, upd) -> case as'mode state of
-    Start -> App.runHandler handleText o
-    WaitingStickerInfo -> App.runHandler handleStickerInfo o
-    WaitingAddEmoji _ -> App.runHandler handleAddEmoji o
+handler :: Handler
+handler = App.UpdateHandler $ \update -> do
+    mode <- gets as'mode
+    case mode of
+        Start -> App.runHandler handleText update
+        WaitingStickerInfo -> App.runHandler handleStickerInfo update
+        WaitingAddEmoji _ -> App.runHandler handleAddEmoji update
 
 example2 :: IO ()
 example2 = do
--- src/Flak/App.hs
+++ src/Flak/App.hs
@@ -1,34 +1,40 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Flak.App where
 
-import qualified Flak.Api as Api
-import qualified Flak.Model as Model
-import qualified Flak.Argument as Argument
+--import Control.Monad.State.Lazy
+import Control.Monad
+import Control.Monad.Trans.State
+import Control.Concurrent ( threadDelay )
 
 import qualified Data.Aeson.Encode.Pretty as P
 import qualified Data.ByteString.Lazy.Char8 as B
 
-import Control.Concurrent ( threadDelay )
-
+import qualified Flak.Api as Api
+import qualified Flak.Model as Model
+import qualified Flak.Argument as Argument
 import Flak.Util (iterateM'', ControlFlow (Continue, Break))
 
-type StatefulHandler s = (s, Model.Update) -> (s, [Api.ApiAction])
+{-
+type StatefulHandler s = Model.Update -> State s [Api.ApiAction]
 type StatelessHandler = Model.Update -> [Api.ApiAction]
 
 makeStateful :: StatelessHandler -> StatefulHandler s
-makeStateful h = \(state, upd) -> (state, h upd)
+makeStateful h = \upd -> state $ \s -> (h upd, s)
+-}
 
 newtype UpdateHandler s = UpdateHandler {
-    runHandler :: (s, Model.Update) -> (s, [Api.ApiAction])
+    runHandler :: Model.Update -> State s [Api.ApiAction]
 }
 
 instance Semigroup (UpdateHandler s) where
-    h1 <> h2 = UpdateHandler $ \(state, upd) ->
-        let (state1, actions1) = runHandler h1 (state, upd) in
-        let (state2, actions2) = runHandler h2 (state1, upd) in
-        (state2, actions1 ++ actions2)
+    h1 <> h2 = UpdateHandler $ \upd -> do
+        actions1 <- runHandler h1 upd
+        actions2 <- runHandler h2 upd
+        pure $ actions1 ++ actions2
 
 instance Monoid (UpdateHandler s) where
-    mempty = UpdateHandler $ \(state, upd) -> (state, [])
+    mempty = UpdateHandler $ \upd -> pure []
 
 data App s = App
     { app'api :: Api.Api
@@ -51,7 +57,7 @@ iterator app state = do
     putStrLn $ B.unpack $ P.encodePretty $ upds
 
     -- Compute new application state and actions
-    let (newState, actions) = concatFoldUpdates (app'handler app) state (Model.result upds)
+    let (actions, newState) = runState (concatFoldUpdates (app'handler app) (Model.result upds)) state
 
     -- Perform actions
     sequence $ map (Api.requestAction (app'api app)) $ actions
@@ -72,10 +78,10 @@ iterator app state = do
 loop :: App s -> s -> IO s
 loop app start = iterateM'' (iterator app) start
 
-concatFoldUpdates :: UpdateHandler s -> s -> [Model.Update] -> (s, [Api.ApiAction])
-concatFoldUpdates f start xs = foldl fun (start, []) xs
+concatFoldUpdates :: forall s. UpdateHandler s -> [Model.Update] -> State s [Api.ApiAction]
+concatFoldUpdates f upds = foldM folder [] upds
     where
-        --fun :: (s, [Api.ApiAction]) -> Model.Update -> (s, [Api.ApiAction])
-        fun (state, actions) upd =
-            let (newState, moreActions) = runHandler f (state, upd) in
-            (newState, actions ++ moreActions)
+        folder :: [Api.ApiAction] -> Model.Update -> State s [Api.ApiAction]
+        folder accumulator upd = do
+            actions <- runHandler f upd
+            pure $ accumulator ++ actions
